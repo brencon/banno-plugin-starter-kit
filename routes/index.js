56e7152a0e5cab240963f27a0cf964ca23a6b919
Brendan Conrad <brendan.conrad@gmail.com>
2020-12-21 07:41:56 -0800

    setup configuration for the title of the external application

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const config = require('config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const title = (config.app.title) ? config.app.title : 'Express';
+  res.render('index', { title: title });
 });
 
 module.exports = router;
