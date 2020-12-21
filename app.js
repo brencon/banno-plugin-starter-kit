2e22e446af7f77ab8339c1d8bd4b1be708ca6020
Brendan Conrad <brendan.conrad@gmail.com>
2020-12-21 08:40:19 -0800

    removed unused users route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 const config = require('config');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 // process env port
 if (config.nodejs.express.port) {
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
