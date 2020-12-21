5aba9b84f32f385b0e6cef5a1382ede77b648f3a
Brendan Conrad <brendan.conrad@gmail.com>
2020-12-21 10:07:59 -0800

    refactoring of banno api paths complete

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const config = require('config');
-const banno = require('../lib/banno.lib');
+const bannoAuth = require('../lib/banno-auth.lib');
+const bannoApi = require('../lib/banno-api.lib');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const routeName = 'index';
   const code = req.query.code;
-  const accessToken = banno.getDecodedToken(routeName, code);
+  //const accessToken = bannoAuth.getDecodedToken(routeName, code);
+  const user = bannoApi.getUser(routeName, code);
   const title = (config.app.title) ? config.app.title : 'Express';
   res.render(routeName, { title: title });
 });
