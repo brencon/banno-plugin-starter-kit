e719311bbc42abce004ecd7774557574f425d5aa
Brendan Conrad <brendan.conrad@gmail.com>
2020-12-21 09:19:15 -0800

    updated with banno lib and initial debug statement

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const config = require('config');
+const banno = require('../lib/banno.lib');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  const accessToken = banno.getDecodedToken();
   const title = (config.app.title) ? config.app.title : 'Express';
   res.render('index', { title: title });
 });
